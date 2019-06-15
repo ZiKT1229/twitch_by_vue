8b70e57a66c722a073b04e5a909a92c4ed8eab4f
KT_Zheng <a6163508@gmail.com>
2019-06-15 14:58:21 +0800

    增加說明

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <main>
     <h1>此為練習 Ajax in Vue 的網頁</h1>
+    <h1>使用的是 <a href="https://www.twitch.tv/" target="_blank">Twitch</a> 所提供的 <a href="https://dev.twitch.tv/docs/v5/" target="_blank">API</a></h1>
+    <h1>使用了: html5, css3, javascript(es6+), Vue, Vue-router, Vue-cli</h1>
+    <h1>此為舊版純 JS 作品=><a href="https://github.com/ZiKT1229/Twitch_API_DEMO" target="_blank">舊版</a></h1>
   </main>
 </template>
 
